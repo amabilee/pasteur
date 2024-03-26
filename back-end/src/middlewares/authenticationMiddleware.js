5c35afb8b214b2797580f3074212356d02dfa365
Gabriel F de  Carvalho <143818850+GabrielGFC@users.noreply.github.com>
2024-03-26 09:05:44 -0300

    git commit

--- back-end/src/middlewares/authenticationMiddleware.js
+++ back-end/src/middlewares/authenticationMiddleware.js
@@ -1,26 +1,3 @@
-<<<<<<< Updated upstream
-function authenticationMiddleware({ requiredRoles }) {
-  return (req, res, next) => {
-    // Verificar se o usuário está autenticado e tem um cargo
-    if (!req.user || !req.user.cargo) {
-      return res.status(401).json({ message: 'Usuário não autenticado ou sem cargo.' });
-    }
-
-    // Verificar se o cargo do usuário corresponde aos cargos necessários
-    const userRole = req.user.cargo;
-    if (requiredRoles.includes(userRole)) {
-      next();
-    } else {
-      return res.status(403).json({ message: 'Acesso negado. Cargo insuficiente.' });
-    }
-  };
-}
-
-export default authenticationMiddleware;
-=======
-// authenticationMiddleware.js
->>>>>>> Stashed changes
-
 import jwt from 'jsonwebtoken';
 
 function authenticationMiddleware(req, res, next) {
